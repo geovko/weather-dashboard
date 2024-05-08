766fa81b743152a276a5be9d22aba70031e96676
geovko <octobermes@gmail.com>
2024-05-08 12:25:13 -0700

    made the base html layout and added some css

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// const currentCity (pull from most recent in local storage)
+
+// function local Storage for history (check for repeats?)
+
+// function getWeather from API
+
+// function render page: (delete previous,) history (buttons), weather
+    // <div id="current" class="inner-box"></div>
+    // <div id="forecast" class="inner-box"></div>
+
+// document ready + event listeners (submit) + history buttons
